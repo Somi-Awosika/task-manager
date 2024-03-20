bf9ab0976c52a82d09705fb7e0d97b1f8a72c184
somi awosika <157589888+Somi-Awosika@users.noreply.github.com>
2024-03-20 18:06:26 +0000

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# Print a message indicating Git's awesomeness
+print("Git is awesome!")
+
+# Prompt the user to enter their name
+user_name = input("Enter your name: ")
+
+# Greet the user with their name
+print("Hello,", user_name)
+
